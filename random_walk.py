16f2daf0595853e8ef42b65af39046ccaabaaa57
franceshoho <francesho.kids@gmail.com>
2020-10-15 17:16:40 -0700

    reorganized codes into new method get_length() to calculate random x and random y in RandomWalk class

--- random_walk.py
+++ random_walk.py
@@ -15,13 +15,8 @@ class RandomWalk:
         dir = [1, -1]  # forward or backward
         length = [0, 1, 2, 3, 4]
         while len(self.x_values) < self.num_points:
-            x_dir = choice(dir)  # choice takes sequence/list/tuple
-            x_step = choice(length)
-            x_length = x_dir * x_step
-
-            y_dir = choice(dir)  # choice takes sequence/list/tuple
-            y_step = choice(length)
-            y_length = y_dir * y_step
+            x_length = self.get_length(dir, length)
+            y_length = self.get_length(dir, length)
 
             if not (x_length == 0 and y_length == 0):
                 # use last stored x, y values to calculate
@@ -29,6 +24,12 @@ class RandomWalk:
                 y = self.y_values[-1] + y_length
                 self.x_values.append(x)
                 self.y_values.append(y)
+
+    def get_length(self, dir, length) :
+        dir = choice(dir)  # choice takes sequence/list/tuple
+        step = choice(length)
+        dist = dir * step
+        return dist
 
     def print_graph(self):
         for i in range(len(self.x_values)):
